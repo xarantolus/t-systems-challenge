9adf7738572d6c7e1ef466a49799357f67cd9bf4
raphaelklein <raphael.klein@tum.de>
2024-11-23 15:52:10 +0100

    InteractiveBoard

--- frontend/src/components/InteractiveBoard.vue
+++ frontend/src/components/InteractiveBoard.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
-
+import FilterDD from "./Dropdowns/FilterDD.vue";
+import ConditionDD from "./Dropdowns/ConditionDD.vue";
 </script>
 
 <template>
+  <nav class="panel">
+    <p class="panel-heading" style="background-color: #e12885; color: white">Filters and Conditions</p>
 
+    <div class="dropdown-container" style="padding: 20px">
+      <FilterDD style="padding: 10px"/>
+      <ConditionDD style="padding: 10px"/>
+    </div>
+
+    <div class="panel-block">
+      <button class="button is-link is-outlined is-fullwidth">
+        Reset all filters
+      </button>
+    </div>
+  </nav>
+  <!-- Container to center dropdowns -->
 </template>
 
 <style scoped>
-
 </style>
