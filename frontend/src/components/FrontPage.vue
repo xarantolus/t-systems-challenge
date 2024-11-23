81bf999a4e1b69cfcc13cf56b023f6941bd9f99c
raphaelklein <raphael.klein@tum.de>
2024-11-23 20:02:23 +0100

    front page update

--- frontend/src/components/FrontPage.vue
+++ frontend/src/components/FrontPage.vue
@@ -45,28 +45,35 @@ export default {
 </script>
 
 <template>
-  <h2 class="title is-1">Y-Router</h2>
-  <div id="flex">
-    <div>
-      <h3 class="title is-3">Create Scenario</h3>
-      <form @submit.prevent="handleSubmit">
-        <div>
-          <label for="numCars">Number of Cars:</label>
-          <input type="number" id="numCars" v-model="numCars" min="1" max="50">
+
+    <h2 class="title is-1">Y-Router</h2>
+    <div id="flex">
+      <div>
+        <div class="box">
+          <h3 class="title is-3">Create Scenario</h3>
+          <form @submit.prevent="handleSubmit">
+            <div>
+              <label for="numCars">Number of Cars:</label>
+              <input type="number" id="numCars" v-model="numCars" min="1" max="50">
+            </div>
+            <div class="input-button-container">
+              <div>
+                <label for="numCustomers">Number of Customers:</label>
+                <input type="number" id="numCustomers" v-model="numCustomers" min="1" max="200">
+              </div>
+              <button type="submit" :class="{'button':true, 'is-loading': creating}">Submit</button>
+            </div>
+          </form>
         </div>
-        <div>
-          <label for="numCustomers">Number of Customers:</label>
-          <input type="number" id="numCustomers" v-model="numCustomers" min="1" max="200">
+      </div>
+      <div class="box">
+        <h3 class="title is-3">Load scenario</h3>
+        <div class="input-button-container">
+          <input type="text" placeholder="UUID" v-model="uuid">
+          <button class="button" @click="handleStart">Start</button>
         </div>
-        <button type="submit" :class="{'button':true, 'is-loading': creating}">Submit</button>
-      </form>
+      </div>
     </div>
-    <div>
-      <h3 class="title is-3">Load scenario</h3>
-      <input type="text" placeholder="UUID" v-model="uuid">
-      <button @click="handleStart">Start</button>
-    </div>
-  </div>
 </template>
 
 <style scoped>
@@ -76,5 +83,17 @@ export default {
   flex-flow: row wrap;
   gap: 2rem;
   justify-items: center;
+}
+
+.button {
+  background-color: #e12885;
+  color: white;
+  margin: 20px;
+}
+
+.input-button-container {
+  display: flex;
+  flex-direction: column;
+  gap: 10px; /* Adjust the gap as needed */
 }
 </style>
