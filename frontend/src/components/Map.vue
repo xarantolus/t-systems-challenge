7022e259efdfde2b4b66e01850dac69080bb4e2c
xarantolus <xarantolus@protonmail.com>
2024-11-24 01:12:13 +0100

    OK this almost works

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -1,30 +1,10 @@
 <template>
   <div class="map-container">
-    <l-map ref="map" v-model:zoom="zoom"
-           :center="(cars.length !== 0 && [cars[0].coordX, cars[0].coordY]) || [48.137154, 11.576124]">
-      <l-tile-layer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          layer-type="base"
-          name="OpenStreetMap"
-      ></l-tile-layer>
-      <l-marker
-          v-for="(marker, index) in interpolateCarPositions()"
-          :key="index"
-          :lat-lng="location(marker)"
-          :icon="getIconUrl(marker)"
-      >
+    <l-map ref="map" v-model:zoom="zoom" :center="(cars.length !== 0 && [cars[0].coordX, cars[0].coordY]) || [48.137154, 11.576124]">
+      <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OpenStreetMap"></l-tile-layer>
+      <l-marker v-for="(marker, index) in interpolateCarPositions()" :key="index" :lat-lng="location(marker)" :icon="getIconUrl(marker)">
         <l-popup>{{ marker.id }}</l-popup>
       </l-marker>
-      <template v-for="(marker, index) in customers">
-        <l-marker
-            v-if="marker.awaitingService"
-            :key="index"
-            :lat-lng="[marker.coordX, marker.coordY]"
-            :icon="personIcon"
-        >
-          <l-popup>{{ marker.id }}</l-popup>
-        </l-marker>
-      </template>
     </l-map>
   </div>
 </template>
@@ -32,7 +12,7 @@
 <script>
 import "leaflet";
 import "leaflet/dist/leaflet.css";
-import {LMap, LMarker, LPopup, LTileLayer} from "@vue-leaflet/vue-leaflet";
+import { LMap, LMarker, LPopup, LTileLayer } from "@vue-leaflet/vue-leaflet";
 import car from "../assets/car.png";
 import fullCar from "../assets/full_car.png";
 import person from "../assets/person.png";
@@ -51,6 +31,7 @@ export default {
   data() {
     return {
       zoom: 12,
+      lastTravelTimes: {},
       carIcon: L.icon({
         iconUrl: car,
         iconSize: [40, 27],
@@ -92,91 +73,94 @@ export default {
       return [obj.location.x, obj.location.y];
     },
     interpolateCarPositions() {
-        let outputs = [];
+      let outputs = [];
 
       let movingCustomers = new Set();
 
-        for (let i = 0; i < this.cars.length; i++) {
-            const car = this.cars[i];
+      for (let i = 0; i < this.cars.length; i++) {
+        const car = this.cars[i];
 
-            if (car.customerId) {
-              // If we have a customer, we group the car + customer together
-              const customer = this.customers.find(customer => customer.id === car.customerId);
-              if (!customer) {
-                throw new Error('Customer not found');
-              }
+        if (car.customerId) {
+          // If we have a customer, we group the car + customer together
+          const customer = this.customers.find(customer => customer.id === car.customerId);
+          if (!customer) {
+            throw new Error('Customer not found');
+          }
 
-              // We calculate the position like this:
-              // if distanceTravelled == 0, the car is driving to the customer,
-              // otherwise the customer is in the car
-              // Calculate as location the progress between the car position and customer position,
-              // or otherwise between car position and customer destination
+          let ltt = this.lastTravelTimes[car.id];
+          let change = false;
+          if (ltt && ltt !== car.activeTime) {
+            console.log('Travel time changed', ltt, car.remainingTravelTime);
+            change = true;
+          }
 
-              if (car.remainingTravelTime) {
-                const progress = car.remainingTravelTime / car.activeTime;
-                if (isNaN(progress)) {
-                  throw new Error('Progress is NaN');
-                }
+          if (car.distanceTravelled === 0 && !change) {
+            this.lastTravelTimes[car.id] = car.activeTime;
 
-                // Here we want the x and y between car and customer destinationX/Y
-                outputs.push({
-                  'type': 'travelling_to_destination',
-                  'car': car,
-                  'customer': customer,
-                  'location': {
-                    x: car.coordX + (car.coordX - customer.destinationX) * progress,
-                    y: car.coordY + (car.coordY - customer.destinationY) * progress,
-                  }
-                })
-                console.log('progress', progress);
-
-                movingCustomers.add(customer.id);
-              } else {
-                const travelProgress = car.remainingTravelTime / car.activeTime;
-                if (isNaN(travelProgress)) {
-                  throw new Error('Travel progress is NaN');
-                }
-
-                // Now essentially take the percentage between the car and the customer
-                outputs.push({
-                  'type': 'travelling_to_customer',
-                  'car': car,
-                  'customer': customer,
-                  'location': {
-                    x: car.coordX + (car.coordX - customer.coordX) * travelProgress,
-                    y: car.coordY + (car.coordY - customer.coordY) * travelProgress,
-                  },
-                });
-              }
-            } else {
-              // If we don't have a customer, we add the car to the cars array
-              outputs.push({
-                'type': 'stationary_car',
-                'car': car,
-                'location': {
-                  x: car.coordX,
-                  y: car.coordY,
-                },
-              });
+            const progress = 1 - car.remainingTravelTime / car.activeTime;
+            if (isNaN(progress)) {
+              throw new Error('Progress is NaN');
             }
-        }
+            console.log('to customer progress', progress);
 
-        // All non-seen customers are added to the outputs array
-        for (let i = 0; i < this.customers.length; i++) {
-          const customer = this.customers[i];
-          if (!movingCustomers.has(customer.id)) {
+            // Interpolating between car and customer destination
             outputs.push({
-              'type': 'stationary_customer',
+              'type': 'travelling_to_customer',
+              'car': car,
               'customer': customer,
               'location': {
-                x: customer.coordX,
-                y: customer.coordY,
+                x: car.coordX + (customer.coordX - car.coordX) * progress,
+                y: car.coordY + (customer.coordY - car.coordY) * progress,
+              }
+            });
+          } else {
+            const travelProgress = 1- (car.remainingTravelTime / car.activeTime);
+            if (isNaN(travelProgress)) {
+              throw new Error('Travel progress is NaN');
+            }
+            console.log('to destination progress', travelProgress);
+            movingCustomers.add(customer.id);
+
+            // Interpolating between car and customer
+            outputs.push({
+              'type': 'travelling_to_destination',
+              'car': car,
+              'customer': customer,
+              'location': {
+                x: car.coordX + (customer.destinationX - car.coordX) * travelProgress,
+                y: car.coordY + (customer.destinationY - car.coordY) * travelProgress,
               },
             });
           }
+        } else {
+          // If we don't have a customer, we add the car to the cars array
+          outputs.push({
+            'type': 'stationary_car',
+            'car': car,
+            'location': {
+              x: car.coordX,
+              y: car.coordY,
+            },
+          });
         }
+      }
 
-        return outputs;
+      // All non-seen customers are added to the outputs array
+      for (let i = 0; i < this.customers.length; i++) {
+        const customer = this.customers[i];
+        if (!movingCustomers.has(customer.id)) {
+          outputs.push({
+            'type': 'stationary_customer',
+            'customer': customer,
+            'location': {
+              x: customer.coordX,
+              y: customer.coordY,
+            },
+          });
+        }
+      }
+
+      return outputs;
     }
   }
 };
